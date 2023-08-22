dbb8b55926545a1b1aded425f54cdab04abd3d8a
Bryce Lindsey <blindsey@verasolutions.org>
2023-08-22 18:18:49 +0530

    updated mapping logic + post logic
    updated mapping logic + post logic

--- jobs/addOrUpdateMemberTags.js
+++ jobs/addOrUpdateMemberTags.js
@@ -1,8 +1,59 @@
-// Contacts to create
-post('/lists/45782', state=> ({members: state.contactsToCreate}));
+//Map Salesforce data to Mailchimp fields
 
+// apiUrl= https://us13.api.mailchimp.com/3.0/lists/{43fead6cd7}
+// auth= anystring:${apikey}
+// payload= {"members":[],"sync_tags":false,"update_existing":false, email_type: "html"}
+// password: ef408eff778772d3739d751a44d7b801-us13
+// curl -X POST \
+//   'https://${dc}.api.mailchimp.com/3.0/lists/{list_id}?skip_merge_validation=<SOME_BOOLEAN_VALUE>&skip_duplicate_check=<SOME_BOOLEAN_VALUE>' \
+//   --user "anystring:${apikey}"' \
+//   -d '{"members":[],"sync_tags":false,"update_existing":false}'
+
+// {
+// "members":[{"subscriber_hash": "qstainland0@php.net",
+// "email_address": "qstainland0@php.net",
+// "merge_fields": {
+//   "FNAME": "Quinlan",
+//   "LNAME": "Stainland"
+// }}],
+// "sync_tags":false,
+// // "update_existing":false,
+// "email_type": "html",
+// "status": "subcribed"
+// }
+
+// fn(state => {
+    
+//     const contactsToCreate = {
+//         "sync_tags":false,
+//         "update_existing":false,
+//         email_type: "html",
+//         members: state.contactsToCreate
+//     }
+
+//     const contactsToUpdate = {
+//         "sync_tags":false,
+//         "update_existing":false,
+//         email_type: "html",
+//         members: state.contactsToUpdate
+//     }
+//     return { ...state, contactsToCreate,contactsToUpdate };
+// });
+
+// Contacts to create
+fn(state => {
+    
+    if(state.contactsToCreate.members.length === 0) {
+        return state;
+    } else {
+        return post('/lists/bf982e5409',state.ContactsToCreate)(state);
+    }
+});
 // Contacts to update
 fn(state => {
-    if(state.contactsToUpdate.length === 0) return state
-    return post('/lists/1213', state=> ({members: state.contactsToUpdate, }))(state);
-})
+    if(state.contactsToUpdate.members.length === 0) {
+        return state;
+    } else {
+        return post('/lists/bf982e5409',state.contactsToUpdate)(state);
+    }
+});
--- jobs/getCampaignMembers.js
+++ jobs/getCampaignMembers.js
@@ -7,7 +7,7 @@ fn(state => {
     return { ...state, lastSyncTime };
 });
 
-// Get campaign members
+// Get campaign members from Salesforce
 query(state=>`
 SELECT Id, Name, FirstName, LastName, Email, CreatedDate,
        Contact.AccountId, Contact.LastModifiedDate, Contact.CreatedDate,
@@ -24,15 +24,39 @@ fn(state => {
 
     console.log(campaignMembers, 'our members')
     // For contacts to create createdDate > state.lastSyncTime
-    const contactsToCreate = [];
-    const contactsToUpdate = [];
+    const contactsToCreate = {
+        "sync_tags":false,
+        "update_existing":false,
+        email_type: "html",
+        members: []
+    };
+    const contactsToUpdate = {
+        "sync_tags":false,
+        "update_existing":false,
+        email_type: "html",
+        members: []
+    };
+
     for (const member of campaignMembers) {
+        const mappedMember = {
+            subscriber_hash: member.Email,
+            email_address: member.Email,
+            full_name: member.fullName,
+            
+            merge_fields: {
+                FNAME: member.FirstName,
+                LNAME: member.LastName,
+                MMERGE4: member.AccountId
+            },
+            tags: [member.Campaign.Nome_da_tag__c]
+        }
         if (member.CreatedDate > state.lastSyncTime) {
-            contactsToCreate.push(member);
+            contactsToCreate.members.push({...mappedMember,status: "subscribed"});
         }
         else {
-            contactsToUpdate.push(member);
+            contactsToUpdate.members.push(mappedMember);
         }
     }
-    return {...state, contactsToUpdate, contactsToCreate}
+    return {...state, references: [], contactsToUpdate, contactsToCreate}
 });
+

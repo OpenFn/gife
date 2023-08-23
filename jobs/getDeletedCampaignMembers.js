f08c337ccb47277e18e44347dcaf5b3015953117
Bryce Lindsey <blindsey@verasolutions.org>
2023-08-23 20:51:03 +0530

    update get + remove tags
    update get + remove tags

--- jobs/getDeletedCampaignMembers.js
+++ jobs/getDeletedCampaignMembers.js
@@ -1,18 +1,20 @@
-// Setup lastSyncTime 
+// Setup lastSyncTime
 fn(state => {
-    const manualCursor = '2023-08-16T15:30:00.000Z'; // SF timestamp
-    console.log(manualCursor, 'manualCursor');
+  const manualCursor = '2023-08-16T15:30:00.000Z'; // SF timestamp
+  console.log(manualCursor, 'manualCursor');
 
-    const lastSyncTime = state.lastRunTime || manualCursor;
-    return { ...state, lastSyncTime };
+  const lastSyncTime = state.lastRunTime || manualCursor;
+  return { ...state, lastSyncTime };
 });
 
 // Get deleted campaign members from Salesforce
-query(state=>`
+query(
+  state => `
 SELECT Nome_da_Campanha__c, Nome_da_Tag__c, Nome_do_Membro_de_Campanha__c, Email__c
 FROM Deleted_Campaign_Member__c
 WHERE CreatedDate > ${state.lastSyncTime}
-`);
+`
+);
 
 //Map Salesforce deleted campaign members to prepare for post to mailchimp
 // fn(state => {
@@ -36,28 +38,35 @@ WHERE CreatedDate > ${state.lastSyncTime}
 //     return { ...state, mappedMembers};
 // });
 
+// {
+//   subscriber_hash: member.Email__c,
+//   tags: [
+//     {
+//       name: member.Nome_da_Tag__c,
+//       status: "inactive",
+//     },
+//   ]
+// }
 fn(state => {
-    const deletedCampaignMembers = state.references[0]['records'];
-    const mappedMembers = [];
-  
-    for (const member of deletedCampaignMembers) {
-      const mappedMember = {
-        subscriber_hash: member.Email__c,
-        tags: [
-          {
-            name: member.Nome_da_Tag__c,
-            status: "inactive",
-          },
-        ]
-      };
-      mappedMembers.push(mappedMember);
-    }
-  
-    //const chunkedMappedMembers = chunk(mappedMembers, 500);
-    return {
-      ...state,
-      references: [],
-      //mappedMembers: chunkedMappedMembers
-      members: [chunk(mappedMembers, 500)]
+  const deletedCampaignMembers = state.references[0]['records'];
+  const mappedMembers = [];
+
+  for (const member of deletedCampaignMembers) {
+    const mappedMember = {
+      is_syncing: true,
+      subscriber_hash: member.Email__c,
+      email_address: member.Email__c,
+      status: 'inactive',
+      tags: [member.Nome_da_Tag__c],
     };
-  });
+    mappedMembers.push(mappedMember);
+  }
+
+  //const chunkedMappedMembers = chunk(mappedMembers, 500);
+  return {
+    ...state,
+    references: [],
+    //mappedMembers: chunkedMappedMembers
+    members: chunk(mappedMembers, 500),
+  };
+});
--- jobs/removeDeletedMemberTags.js
+++ jobs/removeDeletedMemberTags.js
@@ -1,4 +1,4 @@
-// //Contacts to deactivate 
+// //Contacts to deactivate
 // each(
 //     'members[*]',
 //     post('/lists/a4e7ea0abc/members/${'subscriber_hash'}/tags', state => ({
@@ -7,13 +7,15 @@
 //     }))
 //   );
 
-each(
-  'members[*]',
-  post(
-      state => `/lists/a4e7ea0abc/members/${state.members.subscriber_hash}/tags`,
-      state => ({
-          is_syncing: true,
-          members: state.data[0]
-      })
-  )
-);
+// /lists/a4e7ea0abc/members/${state.members.subscriber_hash}/tags
+fn(async state => {
+  for (const members of state.members) {
+    await post('/lists/a4e7ea0abc', state => ({
+      sync_tags: true,
+      update_existing: true,
+      members: members,
+    }))(state).catch(e=> {
+      console.log(er)
+    });
+  }
+});

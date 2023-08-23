4d69aa2abd526490101a1c8eac04b8c43cb79bf9
Bryce Lindsey <blindsey@verasolutions.org>
2023-08-23 19:52:23 +0530

    updates to post
    post

--- jobs/removeDeletedMemberTags.js
+++ jobs/removeDeletedMemberTags.js
@@ -1,15 +1,19 @@
-//Contacts to deactivate 
-// fn(state => {
-//     if (state.mappedMembers.length === 0) {
-//         return state;
-//     } else {
-//         return post('/lists/a4e7ea0abc', state.mappedMembers)(state);
-//     }
-// });
+// //Contacts to deactivate 
+// each(
+//     'members[*]',
+//     post('/lists/a4e7ea0abc/members/${'subscriber_hash'}/tags', state => ({
+//       is_syncing: true,
+//       members: state.data[0]
+//     }))
+//   );
+
 each(
-    'members[*]',
-    post('/lists/a4e7ea0abc', state => ({
-      is_syncing: true,
-      members: state.data[0]
-    }))
-  );
+  'members[*]',
+  post(
+      state => `/lists/a4e7ea0abc/members/${state.members.subscriber_hash}/tags`,
+      state => ({
+          is_syncing: true,
+          members: state.data[0]
+      })
+  )
+);

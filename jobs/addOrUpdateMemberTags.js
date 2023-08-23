299bb258b9fd396551054c5e5e99e9ed6adbe75f
Emmanuel Evance <mtuchidev@gmail.com>
2023-08-23 09:36:14 +0300

    remove the api key comment

--- jobs/addOrUpdateMemberTags.js
+++ jobs/addOrUpdateMemberTags.js
@@ -1,12 +1,3 @@
-// apiUrl= https://us13.api.mailchimp.com/3.0/lists/{43fead6cd7}
-// auth= anystring:${apikey}
-// payload= {"members":[],"sync_tags":false,"update_existing":false, email_type: "html"}
-// password: ef408eff778772d3739d751a44d7b801-us13
-// curl -X POST \
-//   'https://${dc}.api.mailchimp.com/3.0/lists/{list_id}?skip_merge_validation=<SOME_BOOLEAN_VALUE>&skip_duplicate_check=<SOME_BOOLEAN_VALUE>' \
-//   --user "anystring:${apikey}"' \
-//   -d '{"members":[],"sync_tags":false,"update_existing":false}'
-
 // Contacts to create
 fn(state => {
     

64a173b14fb964c4771a2669bebb34d29586853f
Emmanuel Evance <mtuchidev@gmail.com>
2023-08-23 17:16:48 +0300

    use for loop instead of each

--- jobs/addOrUpdateMemberTags.js
+++ jobs/addOrUpdateMemberTags.js
@@ -1,13 +1,16 @@
 // Add or Update members to Mailchimp
-each(
-  'members[*]',
-  post('/lists/a4e7ea0abc', state => ({
-    sync_tags: false,
-    update_existing: true,
-    email_type: 'html',
-    members: state.data[0],
-  }))
-);
+fn(state => {
+  // Check if we do not have data to sync
+  if (state.members.flat().length === 0) return state;
+  for (const membersChunk of state.members) {
+    return post('/lists/a4e7ea0abc', {
+      sync_tags: false,
+      update_existing: true,
+      email_type: 'html',
+      members: membersChunk,
+    })(state);
+  }
+});
 
 // Cleaning up state
 fn(state => ({ ...state, response: {} }));

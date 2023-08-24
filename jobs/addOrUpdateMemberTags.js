d2f01128791885a2d13097ca5319e87da5e9b418
Bryce Lindsey <blindsey@verasolutions.org>
2023-08-24 20:58:08 +0530

    replace async with each
    replace async with each

--- jobs/addOrUpdateMemberTags.js
+++ jobs/addOrUpdateMemberTags.js
@@ -1,17 +1,11 @@
 // Add or Update members to Mailchimp
-fn(async state => {
-  // Check if we do not have data to sync
-  if (state.members.flat().length === 0) return state;
-  for (const membersChunk of state.members) {
-    console.log(`upserting ${membersChunk.length} records`);
-    await post('/lists/a4e7ea0abc', {
-      sync_tags: false,
-      update_existing: true,
-      email_type: 'html',
-      members: membersChunk,
-    })(state);
-  }
-});
-
-// Cleaning up state
-// fn(state => ({ ...state, response: {} }));
+each(
+  'membersToUpdate[*]',
+  'membersToCreate[*]',
+  post('/lists/a4e7ea0abc', state => ({
+    sync_tags: false,
+    update_existing: true,
+    email_type: 'html',
+    members: state.data,
+  }))
+);

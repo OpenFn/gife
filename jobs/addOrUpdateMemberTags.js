96fac857e39fa96ee637bc58ad4342a3d4ad8e8b
Bryce Lindsey <blindsey@verasolutions.org>
2023-08-25 12:36:50 +0530

    correct members array
    update based on what is actually returned in state

--- jobs/addOrUpdateMemberTags.js
+++ jobs/addOrUpdateMemberTags.js
@@ -1,7 +1,6 @@
 // Add or Update members to Mailchimp
 each(
-  'membersToUpdate[*]',
-  'membersToCreate[*]',
+  'members[*]',
   post('/lists/a4e7ea0abc', state => ({
     sync_tags: false,
     update_existing: true,

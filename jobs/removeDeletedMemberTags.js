2ead695adbf76634e688005687f1cdf5b2b17ad7
Emmanuel Evance <mtuchidev@gmail.com>
2023-08-28 16:16:35 +0300

    add error handling

--- jobs/removeDeletedMemberTags.js
+++ jobs/removeDeletedMemberTags.js
@@ -8,3 +8,13 @@ each(
     members: state.data,
   }))
 );
+
+// Alert admin if response has errors
+fn(state => {
+  // Check if response has errors
+  const { errors, error_count } = state.response;
+  if (error_count > 0) {
+    throw new Error(JSON.stringify(errors, null, 2));
+  }
+  return state;
+});

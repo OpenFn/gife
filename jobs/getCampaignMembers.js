9dffa5d56e4943166807feea0d877cbdd5f3971b
Bryce Lindsey <80435316+bryceVeraSolutions@users.noreply.github.com>
2023-09-01 02:16:13 +0530

    wavila@verasolutions.org's change on OpenFn.org

--- jobs/getCampaignMembers.js
+++ jobs/getCampaignMembers.js
@@ -35,20 +35,19 @@ fn(state => {
       subscriber_hash: member.Email,
       email_address: member.Email,
       full_name: member.fullName,
-      status: 'subscribed',
       merge_fields: {
         FNAME: member.FirstName,
         LNAME: member.LastName,
         MMERGE4: member.AccountId,
       },
       tags: [member.Campaign.Nome_da_tag__c],
-    }//;
+    };
     //if (member.CreatedDate > state.lastSyncTime) {
-    //if (member.LastModifiedDate > state.lastSyncTime) {
-    //  membersToCreate.push({ ...mappedMember, status: 'subscribed' });
-    //} else {
-    //  membersToUpdate.push(mappedMember);
-    //}
+    if (member.Contact.LastModifiedDate > state.lastSyncTime) {
+      membersToCreate.push({ ...mappedMember, status: 'subscribed' });
+    } else {
+      membersToUpdate.push(mappedMember);
+    }
   }
 
   return {

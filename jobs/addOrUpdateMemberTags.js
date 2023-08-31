f8a9175fd6fa009ae0e9c734f6e80d4a6855c658
Bryce Lindsey <80435316+bryceVeraSolutions@users.noreply.github.com>
2023-09-01 01:29:16 +0530

    wavila@verasolutions.org's change on OpenFn.org

--- jobs/addOrUpdateMemberTags.js
+++ jobs/addOrUpdateMemberTags.js
@@ -1,7 +1,8 @@
 // Add or Update members to Mailchimp
 each(
   'members[*]',
-  post('/lists/a4e7ea0abc', state => ({
+//  post('/lists/a4e7ea0abc', state => ({
+    post('/lists/a1262d3eab', state => ({
     sync_tags: false,
     update_existing: true,
     email_type: 'html',

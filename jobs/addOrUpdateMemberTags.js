aed9f98cb33fbaecc6d95f6171195570f9afd361
Bryce Lindsey <80435316+bryceVeraSolutions@users.noreply.github.com>
2023-09-13 16:35:50 +0530

    mtuchi@openfn.org's change on OpenFn.org

--- jobs/addOrUpdateMemberTags.js
+++ jobs/addOrUpdateMemberTags.js
@@ -1,38 +1,4 @@
 // Add or Update members to Mailchimp
-// each(
-//   'members[*]',
-//   post(
-//     '/lists/a1262d3eab',
-//     state => ({
-//       sync_tags: false,
-//       update_existing: true,
-//       email_type: 'html',
-//       members: state.data,
-//     }),
-//     {},
-//     state => {
-//       state.chunkErrors.push(state.response.errors);
-//       return state;
-//     }
-//   )
-// );
-
-// // Alert admin if response has errors
-// fn(state => {
-//   // Check if chunks response has errors
-//   const chunkErrors = state.chunkErrors.flat();
-//   if (chunkErrors.length > 0) {
-//     throw new Error(JSON.stringify(chunkErrors, null, 2));
-//   }
-//   return state;
-// });
-
-// // Add or Update members to Mailchimp
-// fn(state => {
-//   console.log(`Upserting ${state.members.length} chunks`);
-//   return state;
-// });
-
 each(
   '$.members[*]',
   post(

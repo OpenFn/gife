d34552baf1156ff1f1e6c61e5fe55b2209fae7fe
Bryce Lindsey <80435316+bryceVeraSolutions@users.noreply.github.com>
2023-09-13 15:27:27 +0530

    mtuchi@openfn.org's change on OpenFn.org

--- jobs/addOrUpdateMemberTags.js
+++ jobs/addOrUpdateMemberTags.js
@@ -1,17 +1,60 @@
 // Add or Update members to Mailchimp
+// each(
+//   'members[*]',
+//   post(
+//     '/lists/a1262d3eab',
+//     state => ({
+//       sync_tags: false,
+//       update_existing: true,
+//       email_type: 'html',
+//       members: state.data,
+//     }),
+//     {},
+//     state => {
+//       state.chunkErrors.push(state.response.errors);
+//       return state;
+//     }
+//   )
+// );
+
+// // Alert admin if response has errors
+// fn(state => {
+//   // Check if chunks response has errors
+//   const chunkErrors = state.chunkErrors.flat();
+//   if (chunkErrors.length > 0) {
+//     throw new Error(JSON.stringify(chunkErrors, null, 2));
+//   }
+//   return state;
+// });
+
+// // Add or Update members to Mailchimp
+// fn(state => {
+//   console.log(`Upserting ${state.members.length} chunks`);
+//   return state;
+// });
+
 each(
-  'members[*]',
+  '$.members[*]',
   post(
     '/lists/a1262d3eab',
-    state => ({
-      sync_tags: false,
-      update_existing: true,
-      email_type: 'html',
-      members: state.data,
-    }),
+    state => {
+      console.log(`Upserting ${state.data.length} members...`);
+      return {
+        sync_tags: false,
+        update_existing: true,
+        email_type: 'html',
+        members: state.data,
+      };
+    },
     {},
     state => {
-      state.chunkErrors.push(state.response.errors);
+      if (state.response.total_created > 0)
+        console.log(`Added ${state.response.total_created} members`);
+      if (state.response.total_updated > 0)
+        console.log(`Updated ${state.response.total_updated} members`);
+
+      if (state.response.error_count > 0)
+        state.chunkErrors.push(state.response.errors);
       return state;
     }
   )
